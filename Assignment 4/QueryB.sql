64430ff39c75b68bc55041d3f72bf6856d6f056d
iumairhasan <umairhasan1456@gmail.com>
2021-05-27 11:04:12 -0400

    Stuff and Rtrim command

--- Assignment 4/QueryB.sql
+++ Assignment 4/QueryB.sql
@@ -15,3 +15,15 @@ from sales.SalesOrderHeader soh
 right join temp
 on temp.customerid = soh.CustomerID
 order by CustomerID desc
+
+--Part c
+select distinct SalesOrderID, 
+stuff((select ', ' + rtrim(cast(ProductID as char))
+	from Sales.SalesOrderDetail so
+	where so.SalesOrderID = sod.SalesOrderID
+	order by ProductID asc
+	for xml path('')), 1, 2, '') as ProductID
+from Sales.SalesOrderDetail sod
+order by sod.SalesOrderID
+
+select * from Sales.SalesOrderDetail
